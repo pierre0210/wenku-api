c541ab87cab902afa007c85ab099d6dbc6f34d15
pierre0210 <gamelauncher0210@gmail.com>
2023-01-28 22:53:56 +0800

    feat: change to GET request & add error handling

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"github.com/pierre0210/wenku-reverseproxy/internal/wenku"
 )
@@ -18,15 +18,17 @@ type Novel struct {
 
 func modifyRequest(r *http.Request) *http.Request {
 	var novel Novel
-	err := json.NewDecoder(r.Body).Decode(&novel)
-	if err != nil {
-		fmt.Println(err.Error())
+	//err := json.NewDecoder(r.Body).Decode(&novel)
+	novel.Aid, _ = strconv.Atoi(r.URL.Query().Get("aid"))
+	novel.Volume, _ = strconv.Atoi(r.URL.Query().Get("vid"))
+
+	volumeList := wenku.GetVolumeList(novel.Aid)
+	if novel.Volume-1 >= len(volumeList) {
+		fmt.Println("Index out of range.")
 		return nil
 	}
-	aid := novel.Aid
-	volumeList := wenku.GetVolumeList(aid)
 	vid := volumeList[(novel.Volume - 1)].Vid
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://dl.wenku8.com/pack.php?aid=%d&vid=%d", aid, vid), nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://dl.wenku8.com/pack.php?aid=%d&vid=%d", novel.Aid, vid), nil)
 	if err != nil {
 		fmt.Println(err.Error())
 		return nil
@@ -36,19 +38,23 @@ func modifyRequest(r *http.Request) *http.Request {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	req := modifyRequest(r)
-	client := http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err.Error())
-		w.Write([]byte(err.Error()))
-	}
+	if req != nil {
+		client := http.Client{}
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err.Error())
+			w.Write([]byte(err.Error()))
+			return
+		}
 
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err.Error())
-		w.Write([]byte(err.Error()))
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println(err.Error())
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Write(body)
 	}
-	w.Write(body)
 }
 
 func main() {
